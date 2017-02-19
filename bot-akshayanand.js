ed2f54a3c4aba2032e984781028fa37e447f72f5
Akshayanand Raut <akshay.raut0303@gmail.com>
2017-02-19 22:55:49 +0530

    Create bot-akshayanand.js
    added file with initial data

---
+++ bot-akshayanand.js
@@ -0,0 +1,35 @@
+var Botkit = require('botkit');
+
+var controller = Botkit.slackbot();
+
+var bot = controller.spawn({
+
+  token: "xoxb-143511011970-HUSJQHVXtWYbqpAJNUABVsmF"
+
+})
+
+bot.startRTM(function(err,bot,payload) {
+
+  if (err) {
+
+    throw new Error('Could not connect to Slack');
+
+  }
+
+});
+
+controller.hears(["Hello","Hi","hii","greetings"],["direct_message","direct_mention","mention","ambient"],function(bot,message) {
+
+  bot.reply(message,'Hello, how may I help you today?');
+
+});
+controller.hears(["Good *"],["direct_message","direct_mention","mention","ambient"],function(bot,message) {
+
+  bot.reply(message,'Hello, Good *');
+
+});
+controller.hears([""],["direct_message","direct_mention","mention","ambient"],function(bot,message) {
+
+  bot.reply(message,'I didn\'t understand what you said. I am not programmed to respond to such requests.');
+
+});
